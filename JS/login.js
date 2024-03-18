080f0d722cf310dfdfc0195322c8768f9140ecd7
ugo <ugo.morelli@etu.univ-amu.fr>
2024-03-18 01:17:26 +0100

    version 1 du panel de login

---
+++ JS/login.js
@@ -0,0 +1,9 @@
+function gotoSignUp(){
+    document.getElementById("signInCard").style.display = "none";
+    document.getElementById("signUpCard").style.display = "grid";
+}
+
+function gotoSignIn(){
+    document.getElementById("signInCard").style.display = "grid";
+    document.getElementById("signUpCard").style.display = "none";
+}
